7282f104ccdbc2a8b61baa01fc27173b07a3c94e
akitakazuki <kazuki.akita@rakus-partners.co.jp>
2022-03-31 17:01:28 +0900

    first commit

---
+++ src/test/java/com/example/mercari/MercariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
